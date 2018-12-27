c1f1992504d62b9d3a84aaea0974b5e487d70cd4
gcamara <gabrielz_C@hotmail.com>
2018-12-27 13:33:31 -0300

    Adicionado texto para busca

--- js/main.js
+++ js/main.js
@@ -1,34 +1,34 @@
 ;(function() {
-    var menu = document.getElementsByClassName('cabecalho')[0]
-    var menuOffset = menu.offsetTop
+    var menu = document.getElementsByClassName('cabecalho')[0];
+    var menuOffset = menu.offsetTop;
 
     window.onscroll = function() {
         if (window.pageYOffset > menuOffset + 80)
-            menu.classList.add('fix')
+            menu.classList.add('fix');
         else
             menu.classList.remove('fix')
-    }
+    };
 
     $('nav a, nav .ic').click(function(event) {
-        var self = $(this)
+        var self = $(this);
         if ($(this).parent('nav').length)
-            self = $(this).parent('nav')
+            self = $(this).parent('nav');
 
-        var section = self.attr('data-section')
-        var el = $('section.'+section)
+        var section = self.attr('data-section');
+        var el = $('section.'+section);
         if (!el.length)
-            el = $(section)
+            el = $(section);
 
         $('html, body').animate({
             scrollTop: el.offset().top - 100
         }, 1000)
-    })
+    });
 
-    $(window).click(event => {
-        var target = $(event.target)
-        var menu = $('menu')
+    $(window).click(function(event) {
+        var target = $(event.target);
+        var menu = $('menu');
         if (target[0] != $('button.trigger-menu')[0] && (target != menu || !target.parent('menu').length)) {
             menu.removeClass('show')
         }
     })
-})()
+})();
